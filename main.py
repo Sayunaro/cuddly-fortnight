1430366f07f20f748166c899fb212848bbcefb60
Sayunaro <165156500+Sayunaro@users.noreply.github.com>
2024-04-13 21:36:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+import random
+
+time.sleep(1)
+print("Добро пожаловать в тест от sayunaro!)")
+q1 = input("Что ты хочешь увидеть на моем гитхабе?")
+if q1 == "Ничего":
+    time.sleep(1)
+    print("А чо сразу ничего то ты ашалел?)")
+else:
+    time.sleep(1)
+    print("Напиши сного в дс skiwy.exe<3")
+while True:
+    time.sleep(2)
+    break
